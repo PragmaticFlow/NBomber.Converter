e4b60efaea2ad5650c791602093d3ad28b088453
User <oleksandr.palamarchuk1310@gmail.com>
2025-02-13 16:31:45 +0200

    Move scenario template code to another file

--- src/NBomber.Converter/ScenarioTemplateProvider.cs
+++ src/NBomber.Converter/ScenarioTemplateProvider.cs
@@ -6,8 +6,9 @@ namespace NBomber.Converter
     {
         public static string GetHARScenarioTemplate(HARFile harFile)
         {
-            string templateContent = "using NBomber.CSharp;\r\n\r\nnamespace Demo.HelloWorld;\r\n\r\npublic class ScenarioWithInit\r\n{\r\n    public void Run()\r\n    {\r\n        var scn1 = Scenario.Create(\"scenario_1\", async context =>\r\n        {\r\n            await Task.Delay(1000);\r\n            return Response.Ok();\r\n        })\r\n\r\n        NBomberRunner\r\n            .RegisterScenarios(scn1)\r\n            .Run();\r\n    }\r\n};";
-            string output = String.Empty;
+            string templateLocation = @"ScenarioTemplates/HelloWorldScenarioTemplate.txt";
+            string templateContent = File.ReadAllText(templateLocation);
+            string outputScenario = String.Empty;
 
             var parser = new FluidParser();
 
@@ -21,7 +22,7 @@ namespace NBomber.Converter
                                    new { model = harFile }, options, true);
                 try
                 {
-                    output = template.Render(ctx);
+                    outputScenario = template.Render(ctx);
                 }
                 catch (Exception ex)
                 {
@@ -33,7 +34,7 @@ namespace NBomber.Converter
                 // handling of the template parsing error
             }
 
-            return output;
+            return outputScenario;
         }
     }
 }
