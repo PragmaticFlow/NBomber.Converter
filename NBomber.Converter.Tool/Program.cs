1be0c71c2545fbaa21d134dd86e41cda7b388138
User <oleksandr.palamarchuk1310@gmail.com>
2025-02-25 15:04:02 +0200

    Add tool logic

---
+++ NBomber.Converter.Tool/Program.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+using NBomber.Converter.Tool;
+using NBomber.Converter;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed<Options>(o =>
+    {
+        var converterType = Path.GetExtension(o.InputFilePath).ToLower();
+        var content = File.ReadAllText(o.InputFilePath);
+
+        switch (converterType)
+        {
+            case ".har":
+                var scenario = HARScenarioConverter.Convert(content);
+                File.WriteAllText(o.OutputFilePath, scenario);
+                break;
+            default:
+                Console.WriteLine("Unknown file type");
+                break;
+        }
+    });
