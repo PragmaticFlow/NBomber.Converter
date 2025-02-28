52a09c887f78e71295073b4f51bf13c2c75d1e62
User <oleksandr.palamarchuk1310@gmail.com>
2025-02-28 21:34:03 +0200

    Add a convert example for the postman collection to the Demo project

--- examples/Demo/Program.cs
+++ examples/Demo/Program.cs
@@ -1,6 +1,11 @@
 ﻿using NBomber.Converter;
 
-var harFileContent = File.ReadAllText("HarExample_15steps.har");
-var scenario = HARScenarioConverter.Convert(harFileContent);
+//var harFileContent = File.ReadAllText("HarExample_15steps.har");
+//var harScenario = HARScenarioConverter.Convert(harFileContent);
 
-File.WriteAllText("HelloWorldScenario.cs", scenario);
+//File.WriteAllText("HelloWorldHarScenario.cs", harScenario);
+
+var postmanCollectionContent = File.ReadAllText("PostmanExample_15steps_v2.0.json");
+var postmanScenario = HARScenarioConverter.Convert(postmanCollectionContent);
+
+File.WriteAllText("HelloWorldPostmanScenario.cs", postmanScenario);
