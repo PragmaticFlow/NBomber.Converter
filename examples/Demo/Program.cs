89f8cf5931781ea9acc3bb152043d18d6b6d28d2
antyadev <antyadev@gmail.com>
2025-04-04 14:32:35 +0300

    fixes

--- examples/Demo/Program.cs
+++ examples/Demo/Program.cs
@@ -1,12 +1,12 @@
 ﻿using NBomber.Converter.HAR;
 using NBomber.Converter.Postman;
 
-var harFileContent = File.ReadAllText("HarExample_4steps.har");
+var harFileContent = File.ReadAllText("HarExample_15steps.har");
 var harScenario = HARScenarioConverter.Convert(harFileContent);
 
 File.WriteAllText("HelloWorldHarScenario.cs", harScenario);
 
-var postmanCollectionContent = File.ReadAllText("PostmanExample_4steps.json");
+var postmanCollectionContent = File.ReadAllText("PostmanExample_15steps_v2.0.json");
 var postmanScenario = PostmanScenarioConverter.Convert(postmanCollectionContent);
 
 File.WriteAllText("HelloWorldPostmanScenario.cs", postmanScenario);
