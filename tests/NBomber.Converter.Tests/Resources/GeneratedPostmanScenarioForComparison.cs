05d7655b4eef0f004ce8325b45d355d6002642e8
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-07 16:56:09 +0300

    Add custom headers for the PostmanScenario example and fix templates for scenarios generation

--- tests/NBomber.Converter.Tests/Resources/GeneratedPostmanScenarioForComparison.cs
+++ tests/NBomber.Converter.Tests/Resources/GeneratedPostmanScenarioForComparison.cs
@@ -11,50 +11,58 @@ public class HelloWorldPostmanExample
 
         var scn1 = Scenario.Create("scenario_1", async context =>
         {
-        
+
             var step1 = await Step.Run("step 1 - SEND SMALL IMAGE", context, async () =>
             {
-                var request = Http.CreateRequest("PUT", "https://clients6.google.com/upload/drive/v2internal/batch?key=AIzaSyD_InbmSFufIEps5UAt2NmB_3LvBH3Sz_8");
-                    
+                var request = Http.CreateRequest("PUT", "https://clients6.google.com/upload/drive/v2internal/batch?key=AIzaSyD_InbmSFufIEps5UAt2NmB_3LvBH3Sz_8")
+                    .WithHeader("Key1", "Value1")
+                    .WithHeader("Key2", "Value2");
+
                 request.WithBody(new StringContent("", Encoding.UTF8, "multipart/form-data"));
-                            
+
                 var response = await Http.Send(httpClient, request);
 
-                return response;        
+                return response;
             });
-        
+
             var step2 = await Step.Run("step 2 - LIST USERS", context, async () =>
             {
-                var request = Http.CreateRequest("GET", "https://reqres.in/api/users?page=2");
-                                
+                var request = Http.CreateRequest("GET", "https://reqres.in/api/users?page=2")
+                    .WithHeader("Key1", "Value1")
+                    .WithHeader("Key2", "Value2");
+
                 var response = await Http.Send(httpClient, request);
 
-                return response;        
+                return response;
             });
-        
+
             var step3 = await Step.Run("step 3 - CREATE RESOURCE", context, async () =>
             {
-                var request = Http.CreateRequest("POST", "https://reqres.in/api/users");
-                    
-                request.WithBody(new StringContent("""{    "name": "morpheus",    "job": "leader"}""", Encoding.UTF8, "application/json"));   
-                            
+                var request = Http.CreateRequest("POST", "https://reqres.in/api/users")
+                    .WithHeader("Key1", "Value1")
+                    .WithHeader("Key2", "Value2");
+
+                request.WithBody(new StringContent("""{    "name": "morpheus",    "job": "leader"}""", Encoding.UTF8, "application/json"));
+
                 var response = await Http.Send(httpClient, request);
 
-                return response;        
+                return response;
             });
-        
+
             var step4 = await Step.Run("step 4 - DELETE RESOURCE", context, async () =>
             {
-                var request = Http.CreateRequest("DELETE", "https://reqres.in/api/users/2");
-                                
+                var request = Http.CreateRequest("DELETE", "https://reqres.in/api/users/2")
+                    .WithHeader("Key1", "Value1")
+                    .WithHeader("Key2", "Value2");
+
                 var response = await Http.Send(httpClient, request);
 
-                return response;        
+                return response;
             });
-         
+
             return Response.Ok();
         })
-            .WithRestartIterationOnFail(shouldRestart: false);
+        .WithRestartIterationOnFail(shouldRestart: false);
 
         NBomberRunner
             .RegisterScenarios(scn1)
